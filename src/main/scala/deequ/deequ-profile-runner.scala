4cc01495a9a6dabab54293e183002e790345007d
Vaquar Khan <vaquar.cna@gmail.com>
2022-01-03 20:42:37 -0600

    Added support for partition data
    Added support for partition data

--- src/main/scala/deequ/deequ-profile-runner.scala
+++ src/main/scala/deequ/deequ-profile-runner.scala
@@ -20,7 +20,8 @@ import org.apache.spark.sql.DataFrame
 
 import scala.collection.JavaConverters._
 import org.apache.spark.sql.{Row, SQLContext}
-import org.apache.spark.sql.types
+import org.apache.spark.sql.types._
+
 
 import com.amazon.deequ.profiles.{ColumnProfilerRunner, NumericColumnProfile}
 
@@ -40,19 +41,43 @@ object GlueApp {
     val args = GlueArgParser.getResolvedOptions(sysArgs, Seq("JOB_NAME",
       "glueDatabase",
       "glueTables",
+	    "pushDownPredicate",
       "targetBucketName").toArray)
 
     Job.init(args("JOB_NAME"), glueContext, args.asJava)
 
     val dbName = args("glueDatabase")
     val tabNames = args("glueTables").split(",").map(_.trim)
+	  //val pushDownPredicate = args("pushDownPredicate")
+    val pushDownPredicate =args("pushDownPredicate").split(",").map(_.trim)
 
-    for (tabName <- tabNames) {
-        val profiler_df = glueContext.getCatalogSource(database = dbName,
-        tableName = tabName,
-        redshiftTmpDir = "",
-        transformationContext = "datasource0").getDynamicFrame().toDF()
+	  //zip values
+    val mapTablePredicate = tabNames zip pushDownPredicate
 
+    //empty df
+    var profiler_df: DataFrame = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], StructType(Seq()))
+
+    //for (tabName <- tabNames) {
+      for((tabName,predicate) <- mapTablePredicate){
+      //println("tabName:=+"+tabName + " -> " +"predicate:="+ predicate)
+
+            if("NONE".equals(predicate)){
+              
+                  profiler_df = glueContext.getCatalogSource(database = dbName, tableName = tabName,redshiftTmpDir = "", transformationContext = "datasource0").getDynamicFrame().toDF()
+                //println("#######################################################")
+                //println("profiler_df NONE="+profiler_df.count())
+                //profiler_df.show()
+                //println("#######################################################")
+            }else{
+              
+                 profiler_df = glueContext.getCatalogSource(database = dbName,tableName = tabName,redshiftTmpDir = "",transformationContext = "datasource0", pushDownPredicate=predicate).getDynamicFrame().toDF()
+                 //println("#######################################################")
+                 //println("profiler_df NOT NONE="+profiler_df.count())
+                 //profiler_df.show()
+                 //println("#######################################################")
+            }
+	
+		
         val profileResult = ColumnProfilerRunner()
         .onData(profiler_df)
         .run()
